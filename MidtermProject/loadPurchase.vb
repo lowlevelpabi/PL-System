997619f7c2a8fee050d2fab61a6947e2f9cc8e4d
lowlevelpabi <ryanandrewreyes16@gmail.com>
2024-07-04 13:52:15 +0800

    Updated few codes

--- MidtermProject/loadPurchase.vb
+++ MidtermProject/loadPurchase.vb
@@ -80,12 +80,13 @@ Public Class loadPurchase
 
     Private Sub PrintPageHandler(sender As Object, e As PrintPageEventArgs)
 
-        Dim offset As Integer = 15
+        Dim targetForm As New checkoutItems
 
-        Dim itemdesc As String = "Item".PadRight(30)
-        Dim Pricedesc As String = "Price".PadRight(8)
-        Dim qtydesc As String = "Qty".PadRight(5)
-        Dim amtdesc As String = "Sub total"
+        Dim offset As Integer = 15
+        Dim itemdesc As String = "Items".PadRight(30)
+        Dim Pricedesc As String = "Price".PadRight(7)
+        Dim qtydesc As String = "Qnt".PadRight(5)
+        Dim amtdesc As String = "Total"
         Dim descline As String = itemdesc + Pricedesc + qtydesc + amtdesc
         e.Graphics.DrawString(descline, New Font("Courier New", 15, FontStyle.Regular), Brushes.Black, 25, 100 + offset)
         offset += 10
@@ -100,7 +101,8 @@ Public Class loadPurchase
 
         Dim productline As String
         For k As Integer = 0 To checkoutItems.DataGridView1.RowCount - 1
-            item = checkoutItems.DataGridView1.Rows(k).Cells(2).Value.ToString.PadRight(30)
+
+            item = checkoutItems.DataGridView1.Rows(k).Cells(1).Value.ToString.PadRight(30)
             price = checkoutItems.DataGridView1.Rows(k).Cells(3).Value.ToString.PadRight(8)
             qty = checkoutItems.DataGridView1.Rows(k).Cells(4).Value.ToString.PadRight(5)
             amount = checkoutItems.DataGridView1.Rows(k).Cells(5).Value.ToString
@@ -131,13 +133,10 @@ Public Class loadPurchase
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
 
-        ' Create a PrintDocument object
         Dim pd As New PrintDocument()
 
-        ' Add PrintPage event handler
         AddHandler pd.PrintPage, AddressOf PrintPageHandler
 
-        ' Display the print preview dialog
         ppd.Document = pd
         ppd.ShowDialog()
 
